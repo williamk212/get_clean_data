0832f6b8eca2bb49711ad37771af1368727443ab
will.keung <williamk212@gmail.com>
2014-06-19 01:32:54 -0400

    working relabeling of headers

--- run_analysis.R
+++ run_analysis.R
@@ -6,8 +6,30 @@
 #
 # run_analysis
 #
+main <- function() {
+  raw_merged_data <- read_data_files_merge()
+  merged_data_header <- label_header(raw_merged_data)
+}
 
-read_files_merge <- function() {
+## This function will set the name of all the header names in
+## the whole data set
+label_header <- function(data_set) {
+  feature_label <- read_header()
+  all_data_header <- c(c("SUBJECT_ID", "ACTIVITY_LABEL_ID"), 
+                       as.character(feature_label$NAME))
+  if (length(colnames(data_set)) != length(all_data_header)) {
+    stop("# of columns of data_set doesn't match read headers") 
+  }
+  names(data_set) <- all_data_header
+  data_set
+}
+
+read_header <- function(feature_label_file = "./UCI_HAR_Dataset/features.txt") {
+  feature_labels <- read.csv(feature_label_file, header = FALSE, 
+                             sep=" ", col.names=c("ID", "NAME"))
+}
+
+read_data_files_merge <- function() {
   subject_file <- "./UCI_HAR_Dataset/train/subject_train.txt"
   y_file <- "./UCI_HAR_Dataset/train/y_train.txt"
   x_file <- "./UCI_HAR_Dataset/train/X_train.txt"
