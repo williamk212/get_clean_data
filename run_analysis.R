1e2cc36e587daffe83d079f5b63182a1c16e38ae
will.keung <williamk212@gmail.com>
2014-06-22 16:09:41 -0400

    all requirements completed. need to write to files

--- run_analysis.R
+++ run_analysis.R
@@ -7,37 +7,107 @@
 # run_analysis
 #
 main <- function() {
+  library(data.table)
+  library(reshape2)
   raw_merged_data <- read_data_files_merge()
-  merged_data_header <- label_header(raw_merged_data)
+  raw_merged_data_DT <- data.table(raw_merged_data)
+  merged_data_header_DT <- label_header(raw_merged_data_DT)
+  
+  filter_col_indices <- get_filter_column_indices(merged_data_header_DT)
+  # obtain only required columns: mean(), std()
+  filtered_data_DT <- merged_data_header_DT[,filter_col_indices, with=FALSE]
+  setkey(filtered_data_DT, ACTIVITY_LABEL_ID)
+
+  # This should give us the data in an expected and organized fashion. 
+  # should meet the following requirements:
+  # 1. Merges the training and the test sets to create one data set.
+  # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+  # 3. Uses descriptive activity names to name the activities in the data set
+  # 4. Appropriately labels the data set with descriptive variable names.
+  wearable_computing_DT <- merge_activity_label(filtered_data_DT)
+  
+  # Now let's reshape data to get average of each variable for 
+  # each activity and each subject.
+  mean_std_cols <- get_mean_std_col(review_data)
+  wearable_computing_DT_melt <- melt(review_data, 
+                                     id=c("SUBJECT_ID", "ACTIVITY_LABEL_NAME"), 
+                                     measure.vars=mean_std_cols)
+  subject_activity_avg <- dcast(wearable_computing_DT_melt, 
+                                SUBJECT_ID+ACTIVITY_LABEL_NAME ~ variable,mean)
+}
+
+merge_activity_label <- function(data_table_set) {
+  activity_label_file <- "./UCI HAR Dataset/activity_labels.txt"
+  activity_labels <- read_ref_table(activity_label_file, 
+                                   names = c("ACTIVITY_LABEL_ID", "ACTIVITY_LABEL_NAME"))
+  activity_labels <- transform(activity_labels, 
+                               ACTIVITY_LABEL_NAME = as.character(ACTIVITY_LABEL_NAME))
+  activity_labels_DT <- data.table(activity_labels)
+  setkey(activity_labels_DT, ACTIVITY_LABEL_ID)
+  activity_col_join <- merge(data_table_set, activity_labels_DT, 
+                             by="ACTIVITY_LABEL_ID")
+  col_join <- activity_col_join
+  # expect to have column length to be 69
+  # if so, then rearrange columns
+  if (ncol(activity_col_join) == 69) {
+    col_join <- activity_col_join[, c(1, 2, 69, 3:68), with=FALSE]
+  } else {
+    print(paste("warning: did not expect ncol for activity_col_join: ", 
+                ncol(activity_col_join)))
+  }
+  col_join
+}
+
+## This function calculates the mean of activity data set based on:
+## subject_id and activity_name
+get_mean_subject_activity <- function(data_table_set, subject_id, activity_name, 
+                                      field_name) {
+  subset_DT <- data_table_set[(data_table_set$SUBJECT_ID==subject_id & 
+                              data_table_set$ACTIVITY_LABEL_NAME==activity_name),]
+  subset_field <- subset_DT[[field_name]]
+  mean(subset_field)
+}
+
+## function will obtain vector of required columns
+get_filter_column_indices <- function(data_set) {
+  mean_std_cols <- get_mean_std_col(data_set)
+  # make sure we grab the first two columns also
+  filter_col_indices <- c("SUBJECT_ID", "ACTIVITY_LABEL_ID", mean_cols, std_cols)
+}
+
+get_mean_std_col <- function(data_set) {
+  mean_cols <- grep(".*-mean\\(\\).*", colnames(data_set), value = TRUE)
+  std_cols <- grep(".*-std\\(\\).*", colnames(data_set), value = TRUE)
+  c(mean_cols, std_cols)  
 }
 
 ## This function will set the name of all the header names in
 ## the whole data set
 label_header <- function(data_set) {
-  feature_label <- read_header()
+  feature_label <- read_ref_table("./UCI HAR Dataset/features.txt")
   all_data_header <- c(c("SUBJECT_ID", "ACTIVITY_LABEL_ID"), 
                        as.character(feature_label$NAME))
   if (length(colnames(data_set)) != length(all_data_header)) {
     stop("# of columns of data_set doesn't match read headers") 
   }
-  names(data_set) <- all_data_header
+  setnames(data_set, all_data_header)
   data_set
 }
 
-read_header <- function(feature_label_file = "./UCI_HAR_Dataset/features.txt") {
-  feature_labels <- read.csv(feature_label_file, header = FALSE, 
-                             sep=" ", col.names=c("ID", "NAME"))
+read_ref_table <- function(table_file, names = c("ID", "NAME")) {
+  reference_table <- read.csv(table_file, header = FALSE, sep=" ", 
+                              col.names=names)
 }
 
 read_data_files_merge <- function() {
-  subject_file <- "./UCI_HAR_Dataset/train/subject_train.txt"
-  y_file <- "./UCI_HAR_Dataset/train/y_train.txt"
-  x_file <- "./UCI_HAR_Dataset/train/X_train.txt"
+  subject_file <- "./UCI HAR Dataset/train/subject_train.txt"
+  y_file <- "./UCI HAR Dataset/train/y_train.txt"
+  x_file <- "./UCI HAR Dataset/train/X_train.txt"
   train_data <- read_files_combine(subject_file, y_file, x_file)
   
-  subject_file <- "./UCI_HAR_Dataset/test/subject_test.txt"
-  y_file <- "./UCI_HAR_Dataset/test/y_test.txt"
-  x_file <- "./UCI_HAR_Dataset/test/X_test.txt"
+  subject_file <- "./UCI HAR Dataset/test/subject_test.txt"
+  y_file <- "./UCI HAR Dataset/test/y_test.txt"
+  x_file <- "./UCI HAR Dataset/test/X_test.txt"
   test_data <- read_files_combine(subject_file, y_file, x_file)
   rbind(train_data, test_data)
 }
@@ -49,4 +119,3 @@ read_files_combine <- function(subject_file_url, y_file_url,
   x_data <- read.table(x_file_url, header = FALSE)
   col_combine_data <- data.frame(subject_data, y_data, x_data)
 }
-
